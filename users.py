bfc44784a8c149bd253a956856752d260c41517b
tbloom9787 <tylerbloom9787@gmail.com>
2020-10-08 18:35:46 -0700

    Initial commit

---
+++ users.py
@@ -0,0 +1,41 @@
+from flask import Flask
+from flask import request, jsonify, g
+import sqlite3
+
+app = Flask(__name__)
+# app.config.from_envvar('APP_CONFIG')
+
+def make_dicts(cursor, row):
+    return dict((cursor.description[idx][0], value)
+                for idx, value in enumerate(row))
+
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect('users.db')
+        db.row_factory = make_dicts
+    return db
+
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+
+
+@app.cli.command('init')
+def init_db():
+    with app.app_context():
+        db = get_db()
+        with app.open_resource('microservices.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
