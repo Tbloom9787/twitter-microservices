380286b66a16e41901d6225adb43d9782027c289
Tyler Bloom <tylerbloom9787@gmail.com>
2020-11-26 13:18:05 -0800

    Finish quick replies and increase random message ID to 1000

--- direct_messaging.py
+++ direct_messaging.py
@@ -6,7 +6,18 @@ def send_dm(recipient, sender , message, quick_replies=None, dynamodb=None):
     if not dynamodb:
         dynamodb = boto3.resource('dynamodb', endpoint_url="http://localhost:8000")
 
-    message_ID = random.randint(0, 100)
+    if quick_replies == 1:
+        message = "Hey, I'll back you back in a minute."
+    elif quick_replies == 2:
+        message = "In class, cannot talk."
+    elif quick_replies == 3:
+        message = "Sounds good!"
+    elif quick_replies == 4:
+        message = "I'll be leaving soon."
+    elif quick_replies == 5:
+        message = "Have a great day!"
+
+    message_ID = random.randint(0, 1000)
 
     table = dynamodb.Table('Messages')
     response = table.put_item(
